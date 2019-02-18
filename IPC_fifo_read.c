835c26c459ece0fc66822c8a200892ea89332c68
lihf <lihf@genvict.com>
2019-02-18 18:42:25 +0800

    IPC fifo(命名管道)进程间通信方式

---
+++ IPC_fifo_read.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>     //read() fuction nead
+
+int main()
+{
+    int  fd;
+    int  len;
+    char buf[1024];
+
+    if(mkfifo("fifo1", 0666) < 0 && errno!=EEXIST) // 创建FIFO管道，创建一个真实存在于文件系统中的文件
+        perror("Create FIFO Failed");
+
+    if((fd = open("fifo1", O_RDONLY)) < 0)  // 以读打开FIFO
+    {
+        perror("Open FIFO Failed");
+        exit(1);
+    }
+    
+    while((len = read(fd, buf, 1024)) > 0) // 读取FIFO管道
+        printf("Read message: %s", buf);
+
+    close(fd);  // 关闭FIFO文件
+    return 0;
+}
